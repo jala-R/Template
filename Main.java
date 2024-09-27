c1270eb6ddf47b16f03a316a10b12db8b580dedf
jalaR <jalagandeswer.r@gmail.com>
2024-09-27 11:33:41 +0530

    Init Commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+package  jalar;
+
+import java.util.*;
+
+
+public  class Main{
+	public static void main(String[] args){
+		Scanner ip = new Scanner(System.in);
+		int t = 1;
+		//t=ip.nextInt();
+		//ip.nextLine();
+		while(t--!=0){
+			solve(ip);
+		}
+	}
+	public static void solve(Scanner ip){
+		System.out.println("running");
+	}
+}
